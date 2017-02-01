684fb6ae4739cd41b72ee06bc8da9581e804b4bd
Luke Wyman <lukewyman@gmail.com>
2017-02-01 02:56:43 -0700

    Issue #10 - created Boot and ServiceSupport for Orchestrator service

--- orchestrator/src/main/scala/com/ecommerce/orchestrator/Boot.scala
+++ orchestrator/src/main/scala/com/ecommerce/orchestrator/Boot.scala
@@ -1,8 +1,13 @@
 package com.ecommerce.orchestrator
 
+import akka.actor.ActorSystem
+import com.ecommerce.orchestrator.api.OrchestratorServiceSupport
+
 /**
   * Created by lukewyman on 1/1/17.
   */
-object Boot extends App {
+object Boot extends App with OrchestratorServiceSupport {
+  implicit val system = ActorSystem("orchestrator")
 
+  start
 }
